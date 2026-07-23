2b569ed427bf9d9787a9cadbad1a3c4cba609af8
Benyameen-cs <benyameen009@gmail.com>
2026-07-23 11:22:42 +0500

    Resgister models in admin.py

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -6,23 +6,20 @@ from ..products.models import Product
 class Order(models.Model):
 
     class StatusChoices(models.TextChoices):
-        PENDING = 'pending' ,'Pending'
-        DELIVERD ='deliverd','Deliverd'
-        SHIPPED = 'shipped','Shipped'
-        CANCELLED = 'cancelled','Cancelled'
+        Pending = 'pending' ,'Pending'
+        Deliverd ='deliverd','Deliverd'
+        Shipped = 'shipped','Shipped'
+        Cancelled = 'cancelled','Cancelled'
 
     customer = models.ForeignKey(Customer , on_delete=models.CASCADE , related_name='order')
     status = models.CharField(
         max_length=50,
         choices=StatusChoices.choices,
-        default=StatusChoices.PENDING
+        default=StatusChoices.Pending
     )
 
     total_price = models.DecimalField(max_digits=10 ,decimal_places=2)
-    order_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Order #{self.id} -- Customer {self.customer.user.username}"
 
 
 class OrderItems(models.Model):
